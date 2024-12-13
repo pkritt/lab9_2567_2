e8235a29adbf05f5d4a0e4ff6874aea48c520c81
pkritt <krittapat37819@gmail.com>
2024-12-13 15:09:42 +0700

    finish

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -1,1 +1,14 @@
+void printO(int M, int N){
+    if (M<=0 || N<=0){cout << "Invalid input";}
 
+    else{
+    for (int i = 0; i < M;i++){
+        for (int j = 0; j < N;j++){
+            cout << 'O';
+        }
+        cout << '\n';
+    }
+    }
+
+
+}
